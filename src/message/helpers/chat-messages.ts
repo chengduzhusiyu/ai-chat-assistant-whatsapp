ec86b379a91529026e523782232e2ba19b55b1dd
chengduzhusiyu <chengduzhusiyu@proton.me>
2024-03-31 15:50:52 +0000

    Typical ashok

--- src/message/helpers/chat-messages.ts
+++ src/message/helpers/chat-messages.ts
@@ -2,4 +2,6 @@ function getHelpMessage(): string {
   return `
     🤖 Bem-vindo! Eu sou o seu assistente virtual. Aqui estão as funcionalidades disponíveis:
     
-    💬 Conversação: Você 
+    💬 Conversação: Você pode conversar comigo e me perguntar qualquer coisa. Basta me enviar uma mensagem!
+        
+    🎨 Ger
