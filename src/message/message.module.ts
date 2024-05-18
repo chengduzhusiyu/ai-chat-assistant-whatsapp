be5a133149752d21fcfba58fd817115666c4b635
chengduzhusiyu <chengduzhusiyu@proton.me>
2024-05-18 10:47:20 +0000

    Add sanity checks

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -1,3 +1,4 @@
 import { Module } from '@nestjs/common';
 import { OpenAiService } from './open-ai/open-ai.service';
-import { Pri
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CustomerService } from './customer/customer.s
