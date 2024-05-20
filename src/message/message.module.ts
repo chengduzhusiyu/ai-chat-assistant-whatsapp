b5b3b723c13f958769b48728ac93fac8f4c0af6c
chengduzhusiyu <chengduzhusiyu@proton.me>
2024-05-20 14:56:07 +0000

    Update README and TODO

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
 import { OpenAiService } from './open-ai/open-ai.service';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CustomerService } from './customer/customer.s
+import { CustomerService } from './customer/customer.service';
+import { MessageService } from './message.service';
+import { RouterService } from './router/rou
