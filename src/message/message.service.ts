93c1d916080d12dbb0dbe0ca683dd60dc9d8fa50
chengduzhusiyu <chengduzhusiyu@proton.me>
2024-05-23 13:41:06 +0000

    Modified config

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -20,3 +20,97 @@ export class MessageService {
     private readonly openAiService: OpenAiService,
     private readonly customerService: CustomerService,
   ) {}
+
+  async createMessage(incomingMessageDto: IncomingMessageDto) {
+    const user = incomingMessageDto.from;
+    const content = incomingMessageDto.body;
+    const customer = await this.customerService.findCustomer(user);
+
+    if (!customer) {
+      const createdCustomer = await this.customerService.createCustomer({
+        user,
+      });
+      if (createdCustomer) {
+        await this.customerService.saveMessage({
+          role: 'system',
+          content: process.env.BOT_PERSONA,
+          owner: {
+            connect: {
+              user,
+            },
+          },
+        });
+        await this.customerService.saveMessage({
+          role: 'user',
+          content,
+          owner: {
+            connect: {
+              user,
+            },
+          },
+        });
+      }
+
+      return getDonationMessage();
+    }
+
+    await this.customerService.saveMessage({
+      role: 'user',
+      content,
+      owner: {
+        connect: {
+          user,
+        },
+      },
+    });
+
+    const context = await this.customerService.getMessagesContext(user);
+    const response = await this.openAiService.createChatCompletition(
+      user,
+      context,
+      content,
+    );
+    if (response === 400) {
+      this.customerService.clearHistory(user);
+      return getServiceUnavailableError();
+    }
+    if (!response || response === 429) {
+      return getServiceUnavailableError;
+    }
+
+    await this.customerService.saveMessage({
+      role: 'assistant',
+      content: response,
+      owner: {
+        connect: {
+          user: user,
+        },
+      },
+    });
+    return response;
+  }
+
+  async createImage(prompt: string) {
+    const createdImage = await this.openAiService.createImage(
+      prompt.substring(9),
+    );
+    return createdImage;
+  }
+
+  async helpMessage() {
+    return getHelpMessage();
+  }
+
+  async clearMessageHistory(user: string) {
+    await this.customerService.clearHistory(user);
+    return getClearChatSuccessMessage();
+  }
+
+  async donationMessage() {
+    return getThankYouMessage();
+  }
+
+  async HelperSuporterMessage() {
+    return getHelperSuporterMessage();
+  }
+}
---
+++ src/message/open-ai/open-ai.service.spec.ts
@@ -0,0 +1,5 @@
+
+import { Test, TestingModule } from '@nestjs/testing';
+import { OpenAiService } from './open-ai.service';
+import { ChatCompletionRequestMessage } from 'openai';
+import dotenv from 'dotenv';
