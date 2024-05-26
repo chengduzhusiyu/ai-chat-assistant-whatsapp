090305d35f2d2f5155173fb48643d7153b0026b6
chengduzhusiyu <chengduzhusiyu@proton.me>
2024-05-26 10:39:59 +0000

    Update teste_bet.user.js

--- src/message/open-ai/open-ai.service.ts
+++ src/message/open-ai/open-ai.service.ts
@@ -2,4 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai';
 @Injectable()
 export class OpenAiService {
-  private
+  private configuration: Configuration;
+  private openAI: OpenAIApi;
+
+  constructor() {
+    this.configuration 
